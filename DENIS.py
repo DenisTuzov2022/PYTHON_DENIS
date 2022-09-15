6eedc34f76467bdc03ad4617b285136682bf9395
DenisTuzov2022 <99098504+DenisTuzov2022@users.noreply.github.com>
2022-09-15 19:58:06 +0300

    denis

--- DENIS.py
+++ DENIS.py
@@ -4,18 +4,22 @@
 # x = 5
 # print(x)
 
+#
+# def plus(a, b):
+#     return a + b
+#
+#
+# text = "new text"
+#
+#
+# def minus(a, b):
+#     return a - b
+#
+#
+# if __name__ == '__main__':
+#     print(plus(2, 2))
+#     print(minus(4, 2))
 
-def plus(a, b):
-    return a + b
 
-
-text = "new text"
-
-
-def minus(a, b):
-    return a - b
-
-
-if __name__ == '__main__':
-    print(plus(2, 2))
-    print(minus(4, 2))
+# output
+print(r"\nhello")

f9ea5650fe51ca9d112250c1f68106d72cb94546
DenisTuzov2022 <99098504+DenisTuzov2022@users.noreply.github.com>
2022-09-16 22:11:23 +0300

    Урок 4

---
+++ Урок 4/task_1.py
@@ -0,0 +1,96 @@
+"""
+Задание 1.
+
+Приведен код, который позволяет сохранить в
+массиве индексы четных элементов другого массива
+
+Сделайте замеры времени выполнения кода с помощью модуля timeit
+
+Попробуйте оптимизировать код, чтобы снизить время выполнения
+Проведите повторные замеры
+
+ОБЯЗАТЕЛЬНО! Добавьте аналитику: что вы сделали и какой это принесло эффект
+"""
+
+from timeit import timeit
+from numpy import array, where
+
+
+# традиционный итератор с функцией append
+def func_1(nums):
+    new_arr = []
+    for i in range(len(nums)):
+        if nums[i] % 2 == 0:
+            new_arr.append(i)
+    return new_arr
+
+
+# списковое включение
+def func_2(nums):
+    return [i for i, el in enumerate(nums) if el % 2 == 0]
+
+
+# 1000
+NUMS = [el for el in range(1000)]
+print(
+    timeit(
+        "func_1(NUMS[:])",
+        globals=globals(),
+        number=1000))
+
+print(
+    timeit(
+        "func_2(NUMS[:])",
+        globals=globals(),
+        number=1000))
+
+
+# 10000
+NUMS = [el for el in range(10000)]
+print("---------------------------------------------------------------------")
+print(
+    timeit(
+        "func_1(NUMS[:])",
+        globals=globals(),
+        number=1000))
+
+print(
+    timeit(
+        "func_2(NUMS[:])",
+        globals=globals(),
+        number=1000))
+
+
+# 100000
+NUMS = [el for el in range(100000)]
+print("---------------------------------------------------------------------")
+print(
+    timeit(
+        "func_1(NUMS[:])",
+        globals=globals(),
+        number=1000))
+
+print(
+    timeit(
+        "func_2(NUMS[:])",
+        globals=globals(),
+        number=1000))
+
+
+
+"""
+Результат:
+---1000---
+0.11754241900000001
+0.09712366
+
+
+---10000---
+1.1843674219999998
+1.1101024599999998
+
+
+---100000---
+13.03590904
+11.307570031000001
+"""
---
+++ Урок 4/task_3.py
@@ -0,0 +1,88 @@
+"""
+Задание 3.
+
+Приведен код, формирующий из введенного числа
+обратное по порядку входящих в него
+цифр и вывести на экран.
+
+Сделайте профилировку каждого алгоритма через timeit
+
+Обязательно предложите еще свой вариант решения!
+
+Сделайте вывод, какая из четырех реализаций эффективнее и почему!!!
+"""
+from timeit import timeit
+from cProfile import run
+
+
+def revers_1(enter_num, revers_num=0):
+    if enter_num == 0:
+        return
+    else:
+        num = enter_num % 10
+        revers_num = (revers_num + num / 10) * 10
+        enter_num //= 10
+        revers_1(enter_num, revers_num)
+
+
+def revers_2(enter_num, revers_num=0):
+    while enter_num != 0:
+        num = enter_num % 10
+        revers_num = (revers_num + num / 10) * 10
+        enter_num //= 10
+    return revers_num
+
+
+def revers_3(enter_num):
+    enter_num = str(enter_num)
+    revers_num = enter_num[::-1]
+    return revers_num
+
+
+def revers_4(enter_num):
+    return ''.join(reversed(str(enter_num)))
+
+
+enter_num = 1234567890
+
+print(
+    'Число наоборот на рекурсиях: ',
+    timeit(
+        f'revers_1({enter_num})',
+        globals=globals()))
+
+print(
+    'Число наоборот на циклах: ',
+    timeit(
+        f'revers_2({enter_num})',
+        globals=globals()))
+
+print(
+    'Число наоборот на срезах: ',
+    timeit(
+        f'revers_3({enter_num})',
+        globals=globals()))
+
+print(
+    'Число наоборот через реверс: ',
+    timeit(
+        f'revers_4({enter_num})',
+        globals=globals()))
+
+run("revers_1(enter_num)")
+run("revers_2(enter_num)")
+run("revers_3(enter_num)")
+run("revers_4(enter_num)")
+
+"""
+Применено 3 вида реализации задачи ( рекурсия, цикл, срез, реверс).
+Согласно полученным результатам видно что срез наиболее быстрый в плане выполнения
+задачи. Рекурсия и цикл имеют арифмитические действия поэтому они проигрывают
+в скорости по сравнению со срезом в котором отсутствуют арифмитические действия.
+Следовательно в подобных задачах оптимально использовать срез.
+
+Число наоборот на рекурсиях:  3.272293691
+Число наоборот на циклах:  2.1066223089999996
+Число наоборот на срезах:  0.41483651099999985
+Число наоборот через реверс:  0.7546048089999999
+"""
